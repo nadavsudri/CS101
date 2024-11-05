38c6b71e4e9577a030a17d0ddef0e0747a30eb04
nadavsudri <hpcity123@gmail.com>
2024-11-05 20:00:25 +0200

    new class added new operation defined first part of task 0; given number k, return 2 prime numbers that obey -> n1+n2 = k; with n1 being the minimal option;

--- calss.java
+++ calss.java
@@ -1,10 +1,38 @@
 public class calss {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        int x =5;
-        for (int i =0; i<5; i++)
-        {
-            System.out.println(x*i);
+    public static boolean isprime(int n){
+        boolean p = false;
+        if (n<=1){return false;}
+        for(int j = 2; j<n;j++){
+            p = n % j == 0;
+            if(p){
+                break;
+            }
         }
+        return (!p);
+    }
+    public static void main(String[] args) {
+        int k = 1222;
+        int counter1 = 0;
+        int [] primes = new int[k];
+        for (int i =0; i <= k;i++ ) {
+            if(isprime(i))
+            {
+                primes[counter1] = i;
+                counter1++;
+            }
+        }
+        for (int i =0; i<primes.length;i++){System.out.print(primes[i]);}
+        System.out.println(isprime(38));
+        int n1=0,n2=0;
+        for (int i =0; i<primes.length;i++){
+            if (primes[i]!=0) {
+                n1 = primes[i];
+                if (isprime(k - n1)) {
+                    n2 = k - n1;
+                    break;
+                }
+            }
+        }
+        System.out.println(n1+" "+n2);
     }
 }
