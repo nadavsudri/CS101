235df4ca587f9b9db1ab9000a0279f9d250c5323
nadavsudri <hpcity123@gmail.com>
2024-11-05 12:46:36 +0200

    new class added new operation defined

--- calss.java
+++ calss.java
@@ -1,5 +1,10 @@
 public class calss {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int x =5;
+        for (int i =0; i<5; i++)
+        {
+            System.out.println(x*i);
+        }
     }
 }
